2a4aeb1769c4a29afd5a07626eed97d097b6a313
Trevor Kimenye <kimenye@gmail.com>
2013-07-11 11:51:40 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orientmobileinsurance::Application.config.secret_token = '97ebe5a560892daf3b6d52a986bf7f4f491069561f588674c2c31355481ada18a42834130daf585f6a6dfcfe54cf71bb5d94aa3c670a7f9b2b86467a59481adc'
