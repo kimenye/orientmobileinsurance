1566128d05b0961279b1cce095d32c29e74b9ea1
Joyce Echessa <jokhessa@yahoo.com>
2013-07-15 15:40:05 +0300

    Added outlet_name to Agent model and included validation

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,5 +1,12 @@
 class Agent < ActiveRecord::Base
 
-  attr_accessible :town, :brand, :outlet, :location, :code, :email, :phone_number
+  validates :town, presence: true
+  validates :brand, presence: true
+  validates :code, presence: true
+  validates :outlet, presence: true
+  validates :location, presence: true
+  validates :outlet_name, presence: true
+
+  attr_accessible :town, :brand, :outlet, :location, :code, :email, :phone_number, :outlet_name
 
 end
