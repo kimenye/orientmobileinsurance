10ee2cbff276c2ce8115ce4b643d338a9e645f3b
Joyce Echessa <jokhessa@yahoo.com>
2013-07-19 11:22:58 +0300

    Modified the status page to display device and policy info

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 require 'premium_service'
 require 'deviceatlasapi'
 
@@ -21,6 +21,8 @@ class MobileController < ApplicationController
     @service = PremiumService.new
 
     @is_insurable = @service.is_insurable(params[:year_of_purchase], params[:sales_agent_code])
+
+    @year_of_purchase = params[:year_of_purchase]
 
     #@device_info = get_device_info
 
