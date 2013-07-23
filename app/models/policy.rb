f82cb6340f24400f95d87d43d75e75304798e30a
Trevor Kimenye <kimenye@gmail.com>
2013-07-23 19:05:00 +0300

    Added rules governing when a customer can make a claim on a policy

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,4 +1,15 @@
 class Policy < ActiveRecord::Base
   belongs_to :quote
   attr_accessible :expiry, :policy_number, :start_date, :status, :quote_id
+
+  def is_open_for_claim
+    days_after_start = Time.now - start_date
+    is_not_expired = expiry - Time.now > 0
+
+    if days_after_start < 21
+      return false
+    else
+      return true && is_not_expired
+    end
+  end
 end
---
+++ test/unit/policy_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class PolicyTest < ActiveSupport::TestCase
+
+  before do
+    @new_policy = Policy.new ({
+        :start_date => Time.now,
+        :expiry => 365.days.from_now
+    })
+
+    @old_policy = Policy.new({
+        :start_date => 22.days.ago,
+        :expiry => (365-22).days.from_now
+    })
+
+    @expired_policy = Policy.new({
+        :start_date => 300.days.ago,
+        :expiry => (1).days.ago
+    })
+  end
+
+  test "A customer cannot make a claim before 21 days after the policy starts" do
+    can_claim = @new_policy.is_open_for_claim
+    assert_equal can_claim, false, "Cannot claim on policy made today"
+  end
+
+  test "A customer can only make a claim after 21 days" do
+    can_claim = @old_policy.is_open_for_claim
+    assert_equal can_claim, true, "Can claim on policy made 22 days ago"
+    end
+
+  test "A customer can only policies that have not expired" do
+    can_claim = @expired_policy.is_open_for_claim
+    assert_equal can_claim, false, "Cannot claim on expired policies"
+  end
+
+end
