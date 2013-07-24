bca8e4b01aed4dc816bae4f8248a98ddc2eeaab9
Trevor Kimenye <kimenye@gmail.com>
2013-07-24 11:17:25 +0300

    removed unnecessary actions

--- app/controllers/enquiry_controller.rb
+++ app/controllers/enquiry_controller.rb
@@ -103,26 +103,4 @@ class EnquiryController < Wicked::WizardController
     @enquiry.save
     redirect_to enquiries_path, :notice => "Enquiry created"
   end
-
-  def secure
-
-  end
-
-  def status_check
-
-    @service = PremiumService.new
-
-    @year_of_purchase = params[:year_of_purchase]
-
-    @sales_agent_code = params[:sales_agent_code]
-
-    @agent = Agent.find_by_code(@sales_agent_code)
-
-    @is_insurable = @service.is_insurable(@year_of_purchase, @sales_agent_code)
-
-    @device_info = get_device_data
-
-    render 'status'
-  end
-
 end
