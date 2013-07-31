4cc7322c539c07f40a87967a77428db0521fd67a
Joyce Echessa <jokhessa@yahoo.com>
2013-07-31 11:31:08 +0300

    Modified the mailer tests

--- test/mailers/customer_mailer_test.rb
+++ test/mailers/customer_mailer_test.rb
@@ -4,21 +4,51 @@ class CustomerMailerTest < ActionMailer::TestCase
 
   after do
     Customer.delete_all
+    Device.delete_all
+    InsuredDevice.delete_all
+    Policy.delete_all
+    Quote.delete_all
+    Claim.delete_all
   end
 
   tests CustomerMailer
-  test "send email" do
+  test "send claim registartion email" do
 
     @customer = Customer.create! :name => "Test", :id_passport => "123456789", :email => "jokhessa@yahoo.com", :phone_number => "254705866564"
+    @device = Device.create! :vendor => "Apple", :model => "IPHONE 5 - 16GB", :marketing_name => "IPHONE 5 - 16GB", :wholesale_price => 100.00, :catalog_price => 150.00
+    @insured_device = InsuredDevice.create! :customer_id => @customer.id, :device_id => @device.id, :imei => "0987654321"
+    @quote = Quote.create! :account_name => "ABCDEFGHI", :insured_device_id => @insured_device.id
+    @policy = Policy.create! :quote_id => @quote.id, :start_date => 365.days.ago
+    @claim = Claim.create! :policy_id => @policy.id, :incident_date => 300.days.ago
 
     # Send the email, then test that it got queued
 
-    email = CustomerMailer.claim_registration(@customer).deliver
+    email = CustomerMailer.claim_registration(@claim).deliver
     assert !ActionMailer::Base.deliveries.empty?
 
     # Test the body of the sent email contains what we expect it to
     assert_equal ['robot@omi.co.ke'], email.from
     assert_equal ['jokhessa@yahoo.com'], email.to
     assert_equal 'OMI Claim Registartion Details', email.subject
+  end
+
+  test "send policy email" do
+
+    @customer = Customer.create! :name => "Test", :id_passport => "123456789", :email => "jokhessa@yahoo.com", :phone_number => "254705866564"
+    @device = Device.create! :vendor => "Apple", :model => "IPHONE 5 - 16GB", :marketing_name => "IPHONE 5 - 16GB", :wholesale_price => 100.00, :catalog_price => 150.00
+    @insured_device = InsuredDevice.create! :customer_id => @customer.id, :device_id => @device.id, :imei => "0987654321"
+    @quote = Quote.create! :account_name => "ABCDEFGHI", :insured_device_id => @insured_device.id
+    @policy = Policy.create! :quote_id => @quote.id, :start_date => 365.days.ago
+    @claim = Claim.create! :policy_id => @policy.id, :incident_date => 300.days.ago
+
+    # Send the email, then test that it got queued
+
+    email = CustomerMailer.policy_purchase(@policy).deliver
+    assert !ActionMailer::Base.deliveries.empty?
+
+    # Test the body of the sent email contains what we expect it to
+    assert_equal ['robot@omi.co.ke'], email.from
+    assert_equal ['jokhessa@yahoo.com'], email.to
+    assert_equal 'OMI Policy Purchase', email.subject
   end
 end
