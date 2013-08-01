678bf2c4e6b0775257ffec550b7d831efa05f41c
Trevor Kimenye <kimenye@gmail.com>
2013-08-01 12:06:14 +0300

    updated the policy object

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -15,6 +15,10 @@ class Policy < ActiveRecord::Base
     end
   end
 
+  def name
+    policy_number
+  end
+
   def is_pending?
     status == "Pending"
   end
@@ -59,7 +63,7 @@ class Policy < ActiveRecord::Base
   def pending_amount
     quote_amount = quote.amount_due
     if quote.premium_type == "Monthly"
-      quote_amount.to_f *= 3
+      quote_amount *= 3
     end
 
     quote_amount.to_f - amount_paid.to_f
