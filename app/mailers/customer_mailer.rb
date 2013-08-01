0f7e25fdddf75646fb0acb36da84535a77e356b8
Joyce Echessa <jokhessa@yahoo.com>
2013-08-01 15:20:14 +0300

    Enabled sending of attachments in email and added the omi banner in the email message

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -4,33 +4,38 @@ class CustomerMailer < ActionMailer::Base
 
   def claim_registration(claim)
     @claim = claim
-    #attachments.inline['admin_banner.jpg'] = File.read('../assets/images/admin_banner.jpg')
+    attachments.inline['admin_banner.jpg'] = File.read("#{Rails.root}/app/assets/images/admin_banner.jpg")
     mail(:to => "#{@claim.policy.quote.insured_device.customer.name} <#{@claim.policy.quote.insured_device.customer.email}>", :subject => "OMI Claim Registartion Details")
   end
 
   def policy_purchase(policy)
     @policy = policy
-    #attachments.inline['admin_banner.jpg'] = File.read('../assets/images/admin_banner.jpg')
+    attachments.inline['admin_banner.jpg'] = File.read("#{Rails.root}/app/assets/images/admin_banner.jpg")
+    attachments['omi.pdf'] = File.read("#{Rails.root}/doc/data/omi.pdf")
     mail(:to => "#{@policy.quote.insured_device.customer.name} <#{@policy.quote.insured_device.customer.email}>", :subject => "OMI Policy Purchase")
   end
 
   def claim_decline(claim)
     @claim = claim
+    attachments.inline['admin_banner.jpg'] = File.read("#{Rails.root}/app/assets/images/admin_banner.jpg")
     mail(:to => "#{@claim.policy.quote.insured_device.customer.name} <#{@claim.policy.quote.insured_device.customer.email}>", :subject => "OMI Claim No. #{@claim.claim_no}")
   end
 
   def reparable_damage_claim(claim)
     @claim = claim
+    attachments.inline['admin_banner.jpg'] = File.read("#{Rails.root}/app/assets/images/admin_banner.jpg")
     mail(:to => "#{@claim.policy.quote.insured_device.customer.name} <#{@claim.policy.quote.insured_device.customer.email}>", :subject => "OMI Claim No. #{@claim.claim_no}")
   end
 
   def irreparable_damage_claim(claim)
     @claim = claim
+    attachments.inline['admin_banner.jpg'] = File.read("#{Rails.root}/app/assets/images/admin_banner.jpg")
     mail(:to => "#{@claim.policy.quote.insured_device.customer.name} <#{@claim.policy.quote.insured_device.customer.email}>", :subject => "OMI Claim No. #{@claim.claim_no}")
   end
 
   def loss_theft_claim(claim)
     @claim = claim
+    attachments.inline['admin_banner.jpg'] = File.read("#{Rails.root}/app/assets/images/admin_banner.jpg")
     mail(:to => "#{@claim.policy.quote.insured_device.customer.name} <#{@claim.policy.quote.insured_device.customer.email}>", :subject => "OMI Claim No. #{@claim.claim_no}")
   end
 
