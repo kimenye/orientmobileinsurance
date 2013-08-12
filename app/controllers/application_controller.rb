868925aa47a2ce7304b75385d7fa452c658def8e
Trevor Kimenye <kimenye@gmail.com>
2013-08-12 16:19:26 +0200

    removed csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  # protect_from_forgery
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
