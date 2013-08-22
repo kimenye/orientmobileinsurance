82e94c47d755201748d08e231691f1d9f267d5cd
Trevor Kimenye <kimenye@gmail.com>
2013-08-22 07:16:04 +0300

    fixed ? on samsung

--- app/controllers/enquiry_controller.rb
+++ app/controllers/enquiry_controller.rb
@@ -113,7 +113,7 @@ class EnquiryController < Wicked::WizardController
         vendor = device_data["vendor"]
         marketingName = device_data["marketingName"]
 
-        invalid_da = (vendor.nil? || vendor.empty) && (model.nil? || model.empty?)
+        invalid_da = (vendor.nil? || vendor.empty?) && (model.nil? || model.empty?)
         puts ">> Invalid match from device atlas : #{invalid_da}"
 
         @enquiry.model = model
