0b9d309d7bda97946eb3c68f9d4285d1597d5402
Trevor Kimenye <kimenye@gmail.com>
2013-09-16 05:46:32 +0300

    updated permissions for admin objects

--- app/admin/claims.rb
+++ app/admin/claims.rb
@@ -4,14 +4,12 @@ ActiveAdmin.register Claim do
     actions :all, :except => [:edit, :destroy]
   end
 
-=begin
-  index do
-    column :claim_no
-    column :claim_type
-    column :incident_date
-    column :policy
-    column :status
-  end
-  actions :index, :show, :delete
-=end
+  #index do
+  #  column :claim_no
+  #  column :claim_type
+  #  column :incident_date
+  #  column :policy
+  #  column :status
+  #end
+  actions :index, :show
 end
--- app/admin/insured_devices.rb
+++ app/admin/insured_devices.rb
@@ -3,5 +3,6 @@ ActiveAdmin.register InsuredDevice do
   controller do
     actions :all, :except => [:edit, :destroy]
   end
-  
+
+  actions :index, :show
 end
