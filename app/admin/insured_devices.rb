f33331c80b25e4c3bc753adcd8ae4560dd6796b6
Joyce Echessa <jokhessa@yahoo.com>
2013-09-20 11:43:17 +0300

    Changed page title for InsuredDevices in ActiveAdmin

--- app/admin/insured_devices.rb
+++ app/admin/insured_devices.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register InsuredDevice do
 
+  index :title => "Insurable Devices"
+  show :title => "Insurable Devices"
+
   controller do
     actions :all, :except => [:edit, :destroy]
   end
