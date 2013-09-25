a541b090d22a4bcb53145448dab72889c4eb40ef
Trevor Kimenye <kimenye@gmail.com>
2013-09-25 15:11:36 +0300

    reverted to old csv

--- app/admin/policies.rb
+++ app/admin/policies.rb
@@ -64,42 +64,42 @@ ActiveAdmin.register Policy do
     column("Section 10") { |p| "" }
   end
 
-  csv do
-    column("TTY") { |p| "N" }
-    column("CODE") { |p| p.policy_number }
-    column("CLIENT NAME") {|p| p.quote.insured_device.customer.name }
-    column("NATIONAL ID") { |p| p.quote.insured_device.customer.id_passport }
-    column("PIN") { |p| 0 }
-    column("POSTAL_ADDRS") { |p| "P.O.Box 00000" }
-    column("POSTAL_TOWN") { |p| "NAIROBI" }
-    column("POSTAL_CODE") { |p| "00000" }
-    column("TELEPHONE NO") { |p| "#{p.quote.insured_device.customer.contact_number.to_s}" }
-    column("MOBILE NO") { |p| "#{p.quote.insured_device.customer.contact_number.to_s}" }
-    column("COUNTRY") { |p| "KENYA" }
-    column("EFF DATE") {|p| p.start_date.to_s(:export) }
-    column("EXP DATE") {|p| p.expiry.to_s(:export) }
-    column("POL NO") {|p| "KOI/OMB/000001/2013" }
-    column("RISK DESCRIPTION") { |p| p.quote.insured_device.device.marketing_name }
-    column("INV DATE") { |p| p.start_date.to_s(:export) }
-    column("INV NO") { |p| "00000" }
-    column("PREMIUM") { |p| "" }
-    column("LEVIES") { |p| "" }
-    column("Reg No") { |p| p.policy_number }
-    column("Make/Model") { |p| p.quote.insured_device.device.marketing_name }
-    column("Model/Type") { |p| p.quote.insured_device.device.marketing_name }
-    column("YOM") { |p| p.quote.insured_device.yop }
-    column("CC rate") { |p| "" }
-    column("Chasis No.") { |p| "" }
-    column("W/screen") { |p| 0 }
-    column("Radio/Stereo") { |p| 0 }
-    column("Cover Type") { |p| "STD" }
-    column("Sum Insured") { |p| p.quote.insured_value }
-    column("Cert No") { |p| "" }
-    column("Section 5") { |p| 0 }
-    column("Section 6") { |p| 0 }
-    column("Section 7") { |p| 0 }
-    column("Section 8") { |p| 0 }
-    column("Section 9") { |p| "" }
-    column("Section 10") { |p| "" }
-  end
+  #csv do
+  #  column("TTY") { |p| "N" }
+  #  column("CODE") { |p| p.policy_number }
+  #  column("CLIENT NAME") {|p| p.quote.insured_device.customer.name }
+  #  column("NATIONAL ID") { |p| p.quote.insured_device.customer.id_passport }
+  #  column("PIN") { |p| 0 }
+  #  column("POSTAL_ADDRS") { |p| "P.O.Box 00000" }
+  #  column("POSTAL_TOWN") { |p| "NAIROBI" }
+  #  column("POSTAL_CODE") { |p| "00000" }
+  #  column("TELEPHONE NO") { |p| "#{p.quote.insured_device.customer.contact_number.to_s}" }
+  #  column("MOBILE NO") { |p| "#{p.quote.insured_device.customer.contact_number.to_s}" }
+  #  column("COUNTRY") { |p| "KENYA" }
+  #  column("EFF DATE") {|p| p.start_date.to_s(:export) }
+  #  column("EXP DATE") {|p| p.expiry.to_s(:export) }
+  #  column("POL NO") {|p| "KOI/OMB/000001/2013" }
+  #  column("RISK DESCRIPTION") { |p| p.quote.insured_device.device.marketing_name }
+  #  column("INV DATE") { |p| p.start_date.to_s(:export) }
+  #  column("INV NO") { |p| "00000" }
+  #  column("PREMIUM") { |p| "" }
+  #  column("LEVIES") { |p| "" }
+  #  column("Reg No") { |p| p.policy_number }
+  #  column("Make/Model") { |p| p.quote.insured_device.device.marketing_name }
+  #  column("Model/Type") { |p| p.quote.insured_device.device.marketing_name }
+  #  column("YOM") { |p| p.quote.insured_device.yop }
+  #  column("CC rate") { |p| "" }
+  #  column("Chasis No.") { |p| "" }
+  #  column("W/screen") { |p| 0 }
+  #  column("Radio/Stereo") { |p| 0 }
+  #  column("Cover Type") { |p| "STD" }
+  #  column("Sum Insured") { |p| p.quote.insured_value }
+  #  column("Cert No") { |p| "" }
+  #  column("Section 5") { |p| 0 }
+  #  column("Section 6") { |p| 0 }
+  #  column("Section 7") { |p| 0 }
+  #  column("Section 8") { |p| 0 }
+  #  column("Section 9") { |p| "" }
+  #  column("Section 10") { |p| "" }
+  #end
 end
