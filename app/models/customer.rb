46494c726ebf1abd5bee876b2aa9b6f24cf12ccd
Trevor Kimenye <kimenye@gmail.com>
2013-10-04 10:29:59 +0300

    Added customer name split logic

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -15,4 +15,26 @@ class Customer < ActiveRecord::Base
       return alternate_phone_number
     end
   end
+
+  def first_name
+    name.split(" ").first
+  end
+
+  def last_name
+    names = name.split(" ")
+    if names.length > 1
+      return names.last
+    else
+      return nil
+    end
+  end
+
+  def middle_name
+    names = name.split(" ")
+    if names.length < 2
+      return nil
+    else
+      return names.slice(1..names.length-2).join(" ")
+    end
+  end
 end
---
+++ test/unit/customer_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class CustomerTest < ActiveSupport::TestCase
+
+  before do
+    @simple = Customer.new :name => "PRISCILLA NJERI KINIIYA"
+  end
+
+  test "The customer names can be split" do
+    assert_equal @simple.first_name, "PRISCILLA"
+    assert_equal @simple.last_name, "KINIIYA"
+    assert_equal @simple.middle_name, "NJERI"
+
+    @simple.name = "Mike"
+    assert_equal @simple.first_name, "Mike"
+    assert_equal @simple.last_name, nil
+    assert_equal @simple.middle_name, nil
+  end
+end
