8a344b50e84501d59ace97a972d803a5e1a93986
Joyce Echessa <jokhessa@yahoo.com>
2013-10-09 13:19:13 +0300

    Added client name, client email and device name to Insurable Devices page

--- app/admin/insured_devices.rb
+++ app/admin/insured_devices.rb
@@ -1,8 +1,32 @@
 ActiveAdmin.register InsuredDevice do
 
   menu :label => "Insurable Devices"
-  index :title => "Insurable Devices"
+  #index :title => "Insurable Devices"
   show :title => "Insurable Devices"
+
+  index :title => "Insurable Devices" do
+    column :customer_id
+    column :device_id
+    column :imei
+    column :created_at
+    column :updated_at
+    column :yop
+    column :phone_number
+
+    column "Customer Name" do |ins_dev|
+      ins_dev.customer.name
+    end
+
+    column "Device Name" do |ins_dev|
+      ins_dev.device.marketing_name
+    end
+
+    column "Customer Email" do |ins_dev|
+      ins_dev.customer.email
+    end
+
+    default_actions
+  end
 
   filter :customer_id
   filter :device_id
