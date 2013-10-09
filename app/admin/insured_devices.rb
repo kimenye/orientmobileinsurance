a40f900bd6f3112056229717155052602b75edc2
Joyce Echessa <jokhessa@yahoo.com>
2013-10-09 12:09:33 +0300

    Added customer name and device name fields to admin Insurable Devices view

--- app/admin/insured_devices.rb
+++ app/admin/insured_devices.rb
@@ -4,6 +4,16 @@ ActiveAdmin.register InsuredDevice do
   index :title => "Insurable Devices"
   show :title => "Insurable Devices"
 
+  filter :customer_id
+  filter :device_id
+  filter :imei
+  filter :created_at
+  filter :updated_at
+  filter :yop
+  filter :phone_number
+  filter :customer_name, :as => :string, :label => "Customer Name"
+  filter :device_marketing_name, :as => :string, :label => "Device Name"
+
   controller do
     actions :all, :except => [:edit, :destroy]
   end
