c2807d690e62af65cb1b1edfb3d13749a750cd25
Joyce Echessa <jokhessa@yahoo.com>
2013-10-10 13:23:21 +0300

    Added customer id, name and tel. no. to admin Quotes page

--- app/admin/quotes.rb
+++ app/admin/quotes.rb
@@ -13,5 +13,20 @@ ActiveAdmin.register Quote do
     column :customer
     column :insured_device
   end
+
+  filter :insured_device
+  filter :agent
+  filter :annual_premium
+  filter :monthly_premium
+  filter :account_name
+  filter :premium_type
+  filter :insured_device_customer_name, :as => :string, :label => "Customer Name"
+  filter :insured_device_customer_id_passport, :as => :string, :label => "ID/Passport No"
+  filter :insured_device_phone_number, :as => :string, :label => "Phone No"
+  filter :expiry_date
+  filter :created_at
+  filter :updated_at
+  filter :insured_value
+
   actions :index
 end
