53d2cf70489399ae9542089e5cb3409c6cf6aea5
Trevor Kimenye <kimenye@gmail.com>
2014-06-05 16:22:34 +0300

    removed unnecessary fields from admin/device

--- app/admin/device.rb
+++ app/admin/device.rb
@@ -20,34 +20,34 @@ ActiveAdmin.register Device do
         model = row[6]
         device_type = row[7]
         catalog_price = row[8]
-        fd_insured_value = row[10]
-        fd_replacement_value = row[17]
-        fd_koil_invoice_value = row[18]
+        # fd_insured_value = row[10]
+        # fd_replacement_value = row[17]
+        # fd_koil_invoice_value = row[18]
 
-        stl_insured_value = row[20]
-        stl_replacement_value = row[27]
-        stl_koil_invoice_value = row[28]
+        # stl_insured_value = row[20]
+        # stl_replacement_value = row[27]
+        # stl_koil_invoice_value = row[28]
 
-        yop_insured_value = row[30]
-        yop_replacement_value = row[37]
-        yop_fd_koil_invoice_value = row[38]
+        # yop_insured_value = row[30]
+        # yop_replacement_value = row[37]
+        # yop_fd_koil_invoice_value = row[38]
 
-        prev_insured_value = row[40]
-        prev_replacement_value = row[47]
-        prev_fd_koil_invoice_value = row[48]
+        # prev_insured_value = row[40]
+        # prev_replacement_value = row[47]
+        # prev_fd_koil_invoice_value = row[48]
 
-        puts "#{stock_code} @ #{catalog_price} - #{fd_koil_invoice_value} - #{prev_fd_koil_invoice_value}"
+        # puts "#{stock_code} @ #{catalog_price} - #{fd_koil_invoice_value} - #{prev_fd_koil_invoice_value}"
         # if Device.find_by_stock_code stock_code
         # end
         device = Device.find_by_stock_code stock_code
         if device.nil?
           # create a new one
           Device.create! :dealer_code => dealer_code, :stock_code => stock_code, :marketing_name => marketing_name, :vendor => vendor,
-            :model => model, :device_type => device_type, :catalog_price => catalog_price, :fd_insured_value => fd_insured_value,
-            :fd_replacement_value => fd_replacement_value, :fd_koil_invoice_value => fd_koil_invoice_value,
-            :yop_insured_value => yop_insured_value, :yop_replacement_value => yop_replacement_value, :yop_fd_koil_invoice_value => yop_fd_koil_invoice_value,
-            :stl_insured_value => stl_insured_value, :stl_replacement_value => stl_replacement_value, :stl_koil_invoice_value => stl_koil_invoice_value,
-            :prev_insured_value => prev_insured_value, :prev_replacement_value => prev_replacement_value, :prev_fd_koil_invoice_value => prev_fd_koil_invoice_value,
+            :model => model, :device_type => device_type, :catalog_price => catalog_price, #:fd_insured_value => fd_insured_value,
+            # :fd_replacement_value => fd_replacement_value, :fd_koil_invoice_value => fd_koil_invoice_value,
+            # :yop_insured_value => yop_insured_value, :yop_replacement_value => yop_replacement_value, :yop_fd_koil_invoice_value => yop_fd_koil_invoice_value,
+            # :stl_insured_value => stl_insured_value, :stl_replacement_value => stl_replacement_value, :stl_koil_invoice_value => stl_koil_invoice_value,
+            # :prev_insured_value => prev_insured_value, :prev_replacement_value => prev_replacement_value, :prev_fd_koil_invoice_value => prev_fd_koil_invoice_value,
             :active => true
         else
           # update everything apart from the model
@@ -57,18 +57,18 @@ ActiveAdmin.register Device do
           device.vendor = vendor
           device.device_type = device_type
           device.catalog_price = catalog_price
-          device.fd_insured_value = fd_insured_value
-          device.fd_replacement_value = fd_replacement_value
-          device.fd_koil_invoice_value = fd_koil_invoice_value
-          device.stl_insured_value = stl_insured_value
-          device.stl_replacement_value = stl_replacement_value
-          device.stl_koil_invoice_value = stl_koil_invoice_value          
-          device.yop_insured_value = yop_insured_value
-          device.yop_replacement_value = yop_replacement_value
-          device.yop_fd_koil_invoice_value = yop_fd_koil_invoice_value          
-          device.prev_insured_value = prev_insured_value
-          device.prev_replacement_value = prev_replacement_value
-          device.prev_fd_koil_invoice_value = prev_fd_koil_invoice_value
+          # device.fd_insured_value = fd_insured_value
+          # device.fd_replacement_value = fd_replacement_value
+          # device.fd_koil_invoice_value = fd_koil_invoice_value
+          # device.stl_insured_value = stl_insured_value
+          # device.stl_replacement_value = stl_replacement_value
+          # device.stl_koil_invoice_value = stl_koil_invoice_value          
+          # device.yop_insured_value = yop_insured_value
+          # device.yop_replacement_value = yop_replacement_value
+          # device.yop_fd_koil_invoice_value = yop_fd_koil_invoice_value          
+          # device.prev_insured_value = prev_insured_value
+          # device.prev_replacement_value = prev_replacement_value
+          # device.prev_fd_koil_invoice_value = prev_fd_koil_invoice_value
           device.active = true
 
           device.save! 
@@ -109,7 +109,7 @@ ActiveAdmin.register Device do
 
   show do |device|
     panel "Device Details" do
-      attributes_table_for device, :vendor, :model, :marketing_name, :stock_code, :active, :dealer_code, :yop_insured_value, :prev_insured_value
+      attributes_table_for device, :vendor, :model, :marketing_name, :stock_code, :active, :dealer_code#, :yop_insured_value, :prev_insured_value
     end
   end
 
