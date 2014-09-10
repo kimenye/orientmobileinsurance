6b1d74bb4393a82f46dafbd9c9c99e722ea4bed5
Trevor Kimenye <kimenye@gmail.com>
2014-09-10 11:17:32 +0300

    removed commented enquiry code and added two tests

---
+++ test/models/enquiry_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class EnquiryTest < ActiveSupport::TestCase
+
+	test "Should strip out any whitespace from insured device phone numbers" do
+	  	enquiry = Enquiry.new(phone_number: " +254 722123456 ", source: "SMS")
+	  	enquiry.save!
+
+	  	enquiry = Enquiry.find(enquiry.id)
+	  	assert_equal "+254722123456", enquiry.phone_number
+	end
+end
---
+++ test/models/insured_device_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class InsuredDeviceTest < ActiveSupport::TestCase
+
+  test "Should strip out any whitespace from insured device phone numbers" do
+  	new_device = InsuredDevice.new(phone_number: " +254 722123456 ")
+  	new_device.save!
+
+  	new_device = InsuredDevice.find(new_device.id)
+  	assert_equal "+254722123456", new_device.phone_number
+  end
+end
