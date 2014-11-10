001ec5a103ed553d8f3abbc36c842d9bd28d1f64
muaad <muadh24@gmail.com>
2014-11-10 16:55:12 +0300

    policy expiry reminders

--- lib/reminder_service.rb
+++ lib/reminder_service.rb
@@ -10,19 +10,27 @@ class ReminderService
       count = 0
 
       policies_in_two_days.each do |policy|
-        # if policy.quote.premium_type == 'Monthly'
-        sms_gateway.send policy.quote.insured_device.phone_number, "Dear #{policy.quote.insured_device.customer.name}, your Orient Mobile premium is due on #{policy.expiry.to_s(:simple)}. Total balance #{ActionController::Base.helpers.number_to_currency(policy.pending_amount, :unit => "KES ", :precision => 0, :delimiter => "")}. Payment due #{ActionController::Base.helpers.number_to_currency(policy.amount_due, :unit => "KES ", :precision => 0, :delimiter => "")}."
-        sms_gateway.send policy.quote.insured_device.phone_number, "Please pay via MPesa (Business No. 530100) or Airtel Money (Business Name JAMBOPAY). Your account no. is #{policy.quote.account_name}"
-        count += 1
-        # end
+        if policy.quote.premium_type == 'Annual'
+          days_to_expiry = (policy.expiry.to_date - Time.now.to_date).to_i
+          sms_gateway.send policy.quote.insured_device.phone_number, "Dear #{policy.quote.insured_device.customer.name}, your Orient Mobile policy is expiring in #{days_to_expiry} #{'day'.pluralize(days_to_expiry)}. To buy a new policy, go to http://omb.korient.co.ke/insure"
+          count += 1
+        else
+          sms_gateway.send policy.quote.insured_device.phone_number, "Dear #{policy.quote.insured_device.customer.name}, your Orient Mobile premium is due on #{policy.expiry.to_s(:simple)}. Total balance #{ActionController::Base.helpers.number_to_currency(policy.pending_amount, :unit => "KES ", :precision => 0, :delimiter => "")}. Payment due #{ActionController::Base.helpers.number_to_currency(policy.amount_due, :unit => "KES ", :precision => 0, :delimiter => "")}."
+          sms_gateway.send policy.quote.insured_device.phone_number, "Please pay via MPesa (Business No. 530100) or Airtel Money (Business Name JAMBOPAY). Your account no. is #{policy.quote.account_name}"
+          count += 1
+        end
       end
 
       policies_today.each do |policy|
-        # if policy.quote.premium_type == 'Monthly'
-        sms_gateway.send policy.quote.insured_device.phone_number, "Dear #{policy.quote.insured_device.customer.name}, your Orient Mobile premium is due today. Total balance #{ActionController::Base.helpers.number_to_currency(policy.pending_amount, :unit => "KES ", :precision => 0, :delimiter => "")}. Payment due #{ActionController::Base.helpers.number_to_currency(policy.amount_due, :unit => "KES ", :precision => 0, :delimiter => "")}. Your account no. is #{policy.quote.account_name}."
-        sms_gateway.send policy.quote.insured_device.phone_number, "Please pay via MPesa (Business No. 530100) or Airtel Money (Business Name JAMBOPAY). Policy lapses at 11:59PM if payment is not made by then."
-        count += 1
-        # end
+        if policy.quote.premium_type == 'Annual'
+          days_to_expiry = (policy.expiry.to_date - Time.now.to_date).to_i
+          sms_gateway.send policy.quote.insured_device.phone_number, "Dear #{policy.quote.insured_device.customer.name}, your Orient Mobile policy is expiring in #{days_to_expiry} #{'day'.pluralize(days_to_expiry)}. To buy a new policy, go to http://omb.korient.co.ke/insure"
+          sms_gateway.send policy.quote.insured_device.phone_number, "Please pay via MPesa (Business No. 530100) or Airtel Money (Business Name JAMBOPAY). Your account no. is #{policy.quote.account_name}"
+          count += 1
+        else
+          sms_gateway.send policy.quote.insured_device.phone_number, "Dear #{policy.quote.insured_device.customer.name}, your Orient Mobile premium is due today. Total balance #{ActionController::Base.helpers.number_to_currency(policy.pending_amount, :unit => "KES ", :precision => 0, :delimiter => "")}. Payment due #{ActionController::Base.helpers.number_to_currency(policy.amount_due, :unit => "KES ", :precision => 0, :delimiter => "")}. Your account no. is #{policy.quote.account_name}."
+          count += 1
+        end
       end
       count
     rescue => error
